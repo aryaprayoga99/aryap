c6f8e17209777bcf78ea1b1c6a30c1b745ae3289
aryaprayoga99 <ap1864434@gmail.com>
2024-12-15 15:27:08 +0700

    Create user-routes.js

---
+++ user-routes.js
@@ -0,0 +1,7 @@
+const userController = require('../controllers/user-controller')
+const express = require('express')
+const router = express. Router()
+
+router.get('/', userController.getAllStudent)
+
+module.exports = router
